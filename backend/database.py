29c3c20654eed76f5f606e19edb9907afb2ae62b
gmanyani527 <131324621+gmanyani527@users.noreply.github.com>
2026-08-31 01:59:05 -0400

    feat: configure PostgreSQL database connection

---
+++ backend/database.py
@@ -0,0 +1,29 @@
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+
+load_dotenv(dotenv_path=".env")
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
+
+if __name__ == "__main__":
+    try:
+        with engine.connect():
+            print("Database connection successful")
+    except Exception as error:
+        print("Database connection failed")
+        print(error)
